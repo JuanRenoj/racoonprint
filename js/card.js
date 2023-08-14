a47cfd6154a03f9231e241f26db5df469717f46a
Juan Renoj <jrenoj@gmail.com>
2023-08-13 19:15:54 -0600

    pagina normal

--- js/card.js
+++ js/card.js
@@ -2,20 +2,26 @@ export function RenderCard(data, divContainer,tipo){
 
     let card=document.createElement("div")
     card.className="card"
-    let nombre=document.createElement('h4');
+    let nombre=document.createElement('h5');
     nombre.className="title"
+    let detalle=document.createElement('p');
+    detalle.className="desc"
     let image=document.createElement("img"); 
     image.className="image-pokemon"
     let btnEnviar=document.createElement('a');
+    let icon=document.createElement("i");
+    icon.className='bx bxl-whatsapp';
     btnEnviar.className="btn-primary"
     let containImage=document.createElement("div");
     containImage.className="contain-image"
    
     nombre.innerHTML=data.nombre;
+    detalle.innerHTML=data.descripcion;
     image.src=data.url;
-    btnEnviar.innerHTML="Detalle";
+ //   btnEnviar.innerHTML="Detalle";
     btnEnviar.id=`btn${tipo}`;
-    btnEnviar.onclick=(e)=>data.onClick(e);
+    btnEnviar.innerHTML=icon
+    btnEnviar.href=`https://wa.me/text=necesito mas informacion de esta imagen ${tipo}:${nombre}&phone=+50247507647`
 
     /*let params=new URLSearchParams()
     params.append("nombre",pokemon.name)
@@ -25,6 +31,7 @@ export function RenderCard(data, divContainer,tipo){
     containImage.append(image)
     card.append(nombre)
     card.append(containImage);
+    card.append(detalle)
     card.append(btnEnviar)
     
     divContainer.appendChild(card)
